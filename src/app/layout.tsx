8949a75c02f0ff6813b3657b3fc64d1ec0c5da9e
Ahmed Fathi <ahmed.fathi.aziz@gmail.com>
2026-05-13 04:22:15 +0400

    update

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -82,10 +82,14 @@ export default function RootLayout({
       className={`${inter.variable} ${cairo.variable} ${kufiArabic.variable} h-full antialiased`}
     >
       <head>
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="" />
+        {/* Icon font: block avoids showing ligature names (search, person…) before the font loads */}
         {/* eslint-disable-next-line @next/next/no-page-custom-font */}
         <link
           rel="stylesheet"
-          href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:wght,FILL@100..700,0..1&display=swap"
+          href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:wght,FILL@100..700,0..1&display=block"
+          fetchPriority="high"
         />
       </head>
       <body className="min-h-full flex flex-col">
